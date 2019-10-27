f5571a6ca20286efff827d531af9c1a06d8a38a0
堀江秀和 <horiehidekazu@horiehidekazunoMacBook-Pro.local>
2019-10-27 16:46:28 +0900

    Reading input date and make domstrings

--- app.js
+++ app.js
@@ -6,15 +6,38 @@ var budgetController =(function() {
 
 var UIController = (function(){
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getinput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                descrition: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+        return DOMstrings;            
+        }
+    };
 })();
 
 
 
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UIController.getDOMstrings();
+
     var ctrlAddItem = function() {
         //1. Get the fiels input date
-
+        var input = UIController.getinput();
+        console.log(input);
         //2. Add the item to the budget controller
 
         //3. Add the item to UI
@@ -23,10 +46,9 @@ var controller = (function(budgetCtrl, UICtrl){
 
         //5. Diplay the budget on the UI
 
-        console.log('aaa');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(e) {
         if (e.keyCode === 13 || e.which === 13){
